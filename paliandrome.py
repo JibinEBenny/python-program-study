b70358698b39482d3cce8f88ce525a8c152c02c6
jibin <jibinbennyofficial@gmail.com>
2024-01-23 10:51:42 +0530

    palinadrome number

---
+++ paliandrome.py
@@ -0,0 +1,11 @@
+num = int(input("Enter the number : "))
+temp = num                     # temp store the number given by the user
+rev = 0
+while num != 0:                # loop ( While )
+    rem = num % 10             # to get the reminder of the number given by the user
+    rev = 10 * rev + rem       # to reverse the string
+    num = num // 10            # the '//' is used because the division should be floor
+if temp == rev:                # compare both number
+ print(temp, "--> Number is palindrome")
+else:
+ print(temp, "--> Not palindrome")
